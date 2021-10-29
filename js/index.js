6bd4c3cd6e5ec6a213f63606d57309776fbf3165
RigobertoWEB <71411324+RigobertoWEB@users.noreply.github.com>
2021-10-29 11:20:21 -0500

    burger menu

---
+++ js/index.js
@@ -0,0 +1,40 @@
+import hamburgerMenu from "./componentes/burguer_menu.js";
+
+hamburgerMenu(".panel-btn", ".panel", ".menu a" )
+
+/*document.getElementById("titulo").style.fontSize = "2rem";
+4
+
+const $hamburguer = document.querySelector(".hamburger");
+//const $navSection = document.querySelector(".navSection");
+
+const $navSection = document.getElementById("navSection");
+
+$hamburguer.addEventListener("click", (e) => {
+    if ($hamburguer.classList.toggle("is-active"))
+
+        $navSection.innerHTML = text;
+
+        else
+        {
+            $navSection.remove();
+        }
+        return;
+
+
+});
+
+let text = `
+<nav>
+<ul>
+<li><a href="#section">Sección 1</a></li>
+<li><a href="#">Sección 2</a></li>
+<li><a href="#">Seccción 3</a></li>
+<li><a href="#">Sección 3</a></li>
+<li><a href="#">Sección 3</a></li>
+</ul>
+</nav>
+
+<section id="section">Hola a todos</section>
+`;
+*/
